cb2fa4360eef8a0b59cef1469ac77b4764d5cc22
Avijit saha <avijitsahashovo2016@gmail.com>
2021-02-28 14:31:17 +0600

    update

---
+++ src/component/Shop/Shop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import fakeData from '../../fakeData'
+const Shop = () => {
+    console.log(fakeData)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Shop;
